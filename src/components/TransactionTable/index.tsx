94227d2fa35b047f74cdb7bc68c44f00c6d088dd
dzampa <dzamparo@gmail.com>
2021-12-09 22:02:35 -0300

    list transactions and seed database

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,11 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction {
+    id: number;
+    title: string;
+    amount: number;
+    type: string;
+    category: string;
+    createAt: string;
+}
+
 function TransactionTable(){
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
     useEffect(() => {
         api.get('transactions')
-            .then(response => console.log(response.data));
+            .then(response => setTransactions(response.data.transactions));
     }, [])
     return (
         <Container>
@@ -19,18 +29,14 @@ function TransactionTable(){
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento de websites</td>
-                        <td className="deposit">R$12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withdraw">-R$1.000</td>
-                        <td>Casa</td>
-                        <td>25/02/2021</td>
-                    </tr>
+                    {transactions.map(transaction => (
+                        <tr key={transaction.id}>
+                            <td>{transaction.title}</td>
+                            <td className={transaction.type}>{transaction.amount}</td>
+                            <td>{transaction.category}</td>
+                            <td>{transaction.createAt}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </Container>
